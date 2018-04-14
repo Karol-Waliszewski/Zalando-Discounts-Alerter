601954fb15e787e568605034ba26dd3d54457a07
Karol-Waliszewski <karolwaliszewski@gmail.com>
2018-04-14 15:09:56 +0200

    Got rid off http

--- app.js
+++ app.js
@@ -6,17 +6,6 @@ const Crawler = require('./controllers/crawler-controller');
 const Requests = require('./controllers/request.controller');
 const config = require('./config');
 
-// Listener
-const http = require('http');
-const port = 2000;
-
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Zalando Discount Alert Server!')
-}
-
-const server = http.createServer(requestHandler)
-
 // Adding new Requests
 Requests.add({
   query: 'https://www.zalando.pl/obuwie-meskie/vans/?sale=true&shoe_fastener=sznurowanie',
@@ -47,10 +36,4 @@ var ZalandoDiscountAlert = function() {
   });
 };
 
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('Something bad happened', err)
-  }
-  console.log(`Server is listening on: ${port}`);
-  ZalandoDiscountAlert();
-});
+ZalandoDiscountAlert();
